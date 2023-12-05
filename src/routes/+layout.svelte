fc737187fca4840c7281e81f59624db0216737e8
suvrowebthree <suvro.ghosh.contact@gmail.com>
2023-12-05 06:21:46 +0530

    Update layout and page components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,19 @@
 	import '@picocss/pico';
 </script>
 
-<slot />
+<article>
+	<header>
+		<nav>
+			<ul>
+				<li><a href="/"><strong>Symphony Sapphires</strong></a></li>
+			</ul>
+			<ul>
+				<li><a href="/">Home</a></li>
+				<li><a href="/create">Create</a></li>
+				<li><a href="/read">Read</a></li>
+			</ul>
+		</nav>
+	</header>
+	<main class="container"><slot /></main>
+	<footer>Symphony Saphhires</footer>
+</article>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	export let data;
+	import Gem from '$lib/components/gem.svelte';
+</script>
+
+{#each data?.gemStones as gem}
+	<span>
+		<article>
+			<h6>{gem.fldtestinglab}-{gem.fldreportnumber}</h6>
+			<p>{gem.fldorigin}-{gem.fldcolor}-{gem.fldspecies}-{gem.fldvariety}</p>
+
+			<p>
+				{gem.fldweightcarats} carats {gem.fldlengthmm} x {gem.fldwidthmm} x {gem.fldheightmm} mm
+			</p>
+
+			{gem.fldtreatment},
+			{gem.fldcut},
+			{gem.fldshape}
+			{#if gem.fldphoto}
+				, Photo: <a href={gem.fldphoto} target="_blank">View</a>
+			{/if}
+			{#if gem.fldvideo}
+				, Video: <a href={gem.fldvideo} target="_blank">View</a>
+			{/if}
+			{#if gem.fldadditionalcomments}
+				, Comments: {gem.fldadditionalcomments}
+			{/if}
+		</article>
+	</span>
+{/each}
+
+<style>
+</style>
